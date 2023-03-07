6d821236abdeaded3a1a2a811e20d0f191b1fc27
R0Y4L23 <subhamroy102.hitece2020@gmail.com>
2023-03-07 10:52:45 +0530

    Background Updated

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <title>Simple Trip</title>
       </Head>
       <div className="home">
-        <Spline scene="https://prod.spline.design/NPYaBDOq6810KpIn/scene.splinecode" className='back3d1' />
+        <Spline scene="https://prod.spline.design/4aqzuzltU8ZaKYqd/scene.splinecode" className='back3d1' />
         <div className="w-4/5 flex flex-row justify-between items-center mx-auto relative z-10 ">
           <Image alt="logo" src={logo} height={150} width={150} />
           <button onClick={() => { router.push("/registration") }} type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Login</button>
